e49ba613ab14dba0db4593f377b6b8ffd793dcbc
Dieter Bruse <dieter@bruse-edv-beratung.de>
2024-11-26 11:53:24 +0100

    Anpassungen für die besser Darstellung und Anzeige von Schaltern die direkt im Mixer eingebunden wurden.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Switch View für Radiomaster TX16S/TX16S II.
 
-[Benötigt EdgeTX 2.7.x oder höher](https://github.com/EdgeTX/edgetx)
+[Benötigt EdgeTX 2.10.x oder höher](https://github.com/EdgeTX/edgetx)
 
 ![Widget](https://github.com/dieterbruse/switchvw/raw/main/Screenshoots/screen01.png)
 
--- main.lua
+++ main.lua
@@ -1,4 +1,4 @@
--- Lua SwitchBox for Beier SFR-1 widget V0.1
+-- Lua SwitchBox for Beier SFR-1 widget V1.2
 --
 --
 -- A Radiomaster TX16S widget for the EdgeTX OS
@@ -28,7 +28,8 @@ local name = "SwitchView"
 local options = {
     --{ "Source", SOURCE, 1 },
     -- BOOL is actually not a boolean, but toggles between 0 and 1
-    --{ "Boolean", BOOL, 1 },
+    { "SwitchName", BOOL, 1 },
+    { "DrawBox", BOOL, 1 },
     --{ "Value", VALUE, 1, 0, 10},
     { "Color", COLOR, BLACK },
     --{ "Text", STRING, "Max8chrs" }
@@ -37,18 +38,25 @@ local options = {
 local inputs = {}
 local BackgroundImage = Bitmap.open("/WIDGETS/SWITCHVW/PNG/RMTX16II.png")
 local log_filename = "/LOGS/SWITCHVWlog.txt"
-
+local firstColumnPx=0
+local secondColumnPx=285
+local firstRowPx=5
+local secondRowPx=40
+local thirdRowPx=75
+local lastRowPx=110
+local SwitchValues = {}
+local PositionForName = 0
 
 local function write_log(message, create)
-      local write_mode = "a"
-        if create ~= true then
-          write_mode = "a"
-        else
-          write_mode = "w"
-        end
-  			local file = io.open(log_filename, write_mode)
-  			io.write(file, message, "\r\n")
-  			io.close(file)
+--      local write_mode = "a"
+--        if create ~= true then
+--          write_mode = "a"
+--        else
+--          write_mode = "w"
+--        end
+--  			local file = io.open(log_filename, write_mode)
+--  			io.write(file, message, "\r\n")
+--  			io.close(file)
 end
 
 local function loadConfig(options)
@@ -57,27 +65,173 @@ local function loadConfig(options)
     local input = 0
     local index = 0
     local theInput = {}
+    local theMix = {}
     local fieldInfo = {}
     inputs = {}
-    while lines > 0 do
-        lines = model.getInputsCount(input)
-        for line=0, lines do
---            write_log("Loading Line Nr. " .. line, true)
-            theInput = model.getInput(input, line)
-            if theInput ~= nil
-            then
---                write_log("Loading Input for Line " .. line .. " Input Name: " .. theInput.name .. " InputName: " .. theInput.inputName,false)
-                fieldInfo = getFieldInfo(theInput.source)
---                write_log("Loading FieldInfo for Line " .. line .. " InputName: " .. fieldInfo.name,false)
-                inputs[index] = {InputID=input, InputLine=theInput, fieldInfo=fieldInfo}
-                index = index + 1
-            end
-        end
-        input = input + 1
+    local delimiter = ""
+
+    SwitchValues = {
+      {
+        name="SA",
+        textEntries=0,
+        firstValue="",
+        secondValue="",
+        col=firstColumnPx,
+        row=lastRowPx
+      },
+      {
+        name="SB",
+        textEntries=0,
+        firstValue="",
+        secondValue="",
+        col=firstColumnPx,
+        row=thirdRowPx
+      },
+      {
+        name="SC",
+        textEntries=0,
+        firstValue="",
+        secondValue="",
+        col=secondColumnPx,
+        row=thirdRowPx
+      },
+      {
+        name="SD",
+        textEntries=0,
+        firstValue="",
+        secondValue="",
+        col=secondColumnPx,
+        row=lastRowPx
+      },
+      {
+        name="SE",
+        textEntries=0,
+        firstValue="",
+        secondValue="",
+        col=firstColumnPx,
+        row=secondRowPx
+      },
+      {
+        name="SF",
+        textEntries=0,
+        firstValue="",
+        secondValue="",
+        col=firstColumnPx,
+        row=firstRowPx
+      },
+      {
+        name="SG",
+        textEntries=0,
+        firstValue="",
+        secondValue="",
+        col=secondColumnPx,
+        row=secondRowPx
+      },
+      {
+        name="SH",
+        textEntries=0,
+        firstValue="",
+        secondValue="",
+        col=secondColumnPx,
+        row=firstRowPx
+      }
+    }
+
+    write_log("Start the Logging for SwitchView Widget V 1.2", true)
+    if options.SwitchName == 1
+    then
+      PositionForName = 29
+    else
+      PositionForName = 0
     end
 
+    for index, value in ipairs(SwitchValues)
+    do
+      write_log("Start the search with Line Nr. " .. index .. " Switch: " .. value.name .. " at Positon ROW/COL: " .. value.row .. "/" .. value.col, false)
+      input = 0
+      lines=99
+      while lines > 0 do
+        lines = model.getInputsCount(input)
+        for line=0, lines do
+--          write_log("Loaded InputLines " .. input .. " Line: " .. line , false)
+          theInput = model.getInput(input, line)
+          if theInput ~= nil
+          then
+--            write_log("Loaded Input Name: " .. theInput.name, false)
+            fieldInfo = getFieldInfo(theInput.source)
+            if string.upper(fieldInfo.name) == string.upper(value.name)
+            then
+--              write_log("Loaded FieldInfo " .. fieldInfo.name, false)
+              if SwitchValues[index].textEntries < 2
+              then
 
-end
+                if options.SwitchName == 1 and SwitchValues[index].textEntries == 0
+                then
+                  SwitchValues[index].firstValue = SwitchValues[index].name .. ":"
+                end
+
+                SwitchValues[index].firstValue = SwitchValues[index].firstValue .. delimiter .. theInput.name
+                delimiter = "/"
+              else
+                if SwitchValues[index].textEntries == 2
+                then
+                  delimiter = ""
+                end
+                SwitchValues[index].secondValue = SwitchValues[index].secondValue .. delimiter .. theInput.name
+                delimiter = "/"
+              end
+              SwitchValues[index].textEntries = SwitchValues[index].textEntries + 1
+            end
+          end
+        end
+        input = input + 1
+        delimiter = ""
+      end
+      input = 0
+      for channel=0, 15 do
+        lines = model.getMixesCount(channel)
+        for line=0, lines do
+          write_log("Loaded MixLines " .. channel .. " Line: " .. line , false)
+          theMix = model.getMix(channel, line)
+          if theMix ~= nil
+          then
+            fieldInfo = getFieldInfo(theMix.source)
+            write_log("Mix CH" .. channel + 1 .. " Name:" .. theMix.name .. " FieldName:" .. fieldInfo.name, false)
+            if string.upper(fieldInfo.name) == string.upper(value.name)
+            then
+              write_log("Loaded FieldInfo " .. fieldInfo.name, false)
+              if SwitchValues[index].textEntries < 2
+              then
+
+                if options.SwitchName == 1 and SwitchValues[index].textEntries == 0
+                then
+                  SwitchValues[index].firstValue = SwitchValues[index].name .. ":"
+                end
+
+                SwitchValues[index].firstValue = SwitchValues[index].firstValue .. delimiter .. theMix.name
+                delimiter = "/"
+              else
+                if SwitchValues[index].textEntries == 2
+                then
+                  delimiter = ""
+                end
+                SwitchValues[index].secondValue = SwitchValues[index].secondValue .. delimiter .. theMix.name
+                delimiter = "/"
+              end
+              SwitchValues[index].textEntries = SwitchValues[index].textEntries + 1
+            end
+          end
+          delimiter = ""
+        end
+      end
+    end
+
+    -- ONLY FOR DEBUG
+    for index, value in ipairs(SwitchValues)
+    do
+      write_log("Finished the search with Line Nr. " .. index .. " 1st Value: " .. value.firstValue .. " 2nd Value: " .. value.secondValue, false)
+    end
+  end
 
 local function create(zone, options)
     -- Runs one time when the widget instance is registered
@@ -109,48 +263,24 @@ local function create(zone, options)
   local function refresh(widget, event, touchState)
     -- Runs periodically only when widget instance is visible
     -- If full screen, then event is 0 or event value, otherwise nil
- 
     lcd.drawBitmap(BackgroundImage, 0, 0)
-    local idx = 0
-    local rightStart = 285
-    for index,value in ipairs({{name="sa",col=0, row=110}, {name="sb",col=0, row=75}, {name="sc",col=rightStart, row=75}, {name="sd",col=rightStart, row=110}, {name="se",col=0, row=40}, {name="sf",col=0, row=5}, {name="sg",col=rightStart, row=40},{name= "sh",col=rightStart, row=5}})
+ 
+    for index, value in ipairs(SwitchValues)
     do
-      local firstLine = ""
-      local secondLine = ""
-      local delimiter = string.upper(value.name) .. ": "
-      local inputsPrinted = 0
-      lcd.drawRectangle(value.col, value.row, 105, 33, RED)      
-      for input_index = 0, #inputs do
---        write_log("View: Compare Input Nr. " .. input_index .. " Name: " .. string.upper(inputs[input_index].fieldInfo.name) .. " with valueName: " .. string.upper(value.name), false)
-        if string.upper(inputs[input_index].fieldInfo.name) == string.upper(value.name)
-        then
-          if inputsPrinted < 2
-          then
-            firstLine = firstLine .. delimiter .. inputs[input_index].InputLine.name
-  --          write_log("View: Input Nr. " .. input_index .. "ResultValue: " .. resultValue, false )
-            delimiter = "/"
-          else
-            if inputsPrinted == 2
-            then
-              delimiter = ""
-            end
-            secondLine = secondLine .. delimiter .. inputs[input_index].InputLine.name
-            delimiter = "/"
-          end
-          inputsPrinted = inputsPrinted + 1
-        end
-      end
-      if firstLine ~= ""
+      write_log("Draw Switch " .. value.name .. " 1st Value: " .. value.firstValue .. " 2nd Value: " .. value.secondValue .. " to Col/Raw " .. value.col .. "/" .. value.row, false)
+      if widget.options.DrawBox == 1
       then
-        lcd.drawText(value.col+1,value.row, firstLine, widget.options.Color)
-        idx = idx + 1
+        lcd.drawRectangle(value.col, value.row, 105, 33, RED)      
       end
-      if secondLine ~= ""
+      if value.firstValue ~= ""
       then
-        lcd.drawText(value.col + 29,value.row + 15, secondLine, widget.options.Color)
+        lcd.drawText(value.col+1,value.row, value.firstValue, widget.options.Color)
+       end
+      if value.secondValue ~= ""
+      then
+        lcd.drawText(value.col + PositionForName,value.row + 15, value.secondValue, widget.options.Color)
       end
     end
-
 end
 
   return {
